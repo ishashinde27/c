273a567081b351a173e10e2c72b8eff94f064956
Isha Shinde <149763675+ishashinde27@users.noreply.github.com>
2026-04-28 06:46:11 +0530

    Add files via upload

---
+++ ParametersX.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void Display(int No1, int No2)
+{
+    printf("%d %d\n",No1,No2);
+}
+
+int main()
+{
+
+    int A = 11, B = 21;
+
+    Display(A,B);      //Call By value
+
+    return 0;
+}
