7eec921b4d42b24f38765d5f59eecae00cd16e34
Isha Shinde <149763675+ishashinde27@users.noreply.github.com>
2026-05-06 12:12:25 +0530

    Add files via upload

---
+++ StructurePointer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+struct Demo
+{
+    int i;      //4
+    int *ptr;   //8
+};
+
+int main()
+{
+    struct Demo dobj;
+
+    printf("size of object is : %lu\n",sizeof(dobj));    //8 OR 16
+
+    return 0;
+
+}
